a91bd5e862d43988449bc3ccf24e776e78a7577d
Milanx64 <markovic37m@gmail.com>
2020-07-06 11:52:01 +0200

    Class Customer has only one property valueOfPurchase

---
+++ MarketStore/src/Customer.java
@@ -0,0 +1,20 @@
+
+public class Customer {
+
+	private double valueOfPurchase;
+	
+	Customer(double valueOfPurchase){
+		this.valueOfPurchase = valueOfPurchase;
+	}
+
+	public double getValueOfPurchase() {
+		return valueOfPurchase;
+	}
+
+	public void setValueOfPurchase(double valueOfPurchase) {
+		this.valueOfPurchase = valueOfPurchase;
+	}
+	
+	
+	
+}
