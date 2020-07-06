7dd70a9e8f2db0c81cd3443a4aa6545ba2fa4097
Milanx64 <markovic37m@gmail.com>
2020-07-06 12:24:57 +0200

    Exception handling added

--- MarketStore/src/Card.java
+++ MarketStore/src/Card.java
@@ -14,37 +14,42 @@ public class Card {
 	
 	//Set discountRate depending on type of card and turnover;
 	Card initializeDiscountRate(Card card) {
-		if(card.getType().equalsIgnoreCase("bronze")) {
-			if(card.getTurnover() < 100) {
-				card.setDiscountRate(0);
-			} else if(card.getTurnover()>=100 && card.getTurnover()<=300) {
-				card.setDiscountRate(1);
-			}else if(card.getTurnover()>300) {
-				card.setDiscountRate(2.5);
-			}
-		}
-		if(card.getType().equalsIgnoreCase("silver")) {
-			if(card.getTurnover() < 300) {
-				card.setDiscountRate(2);
-			} else if(card.getTurnover() >= 300) {
-				card.setDiscountRate(3.5);
-			}
-		}
-		if(card.getType().equalsIgnoreCase("gold")) {
-			double discountGrow;
-			int newDiscount;
-			discountGrow = card.getTurnover() /100;
-			card.setDiscountRate(2);
-			if(card.getTurnover() >= 100) {
-				if(discountGrow >= 10) {
-					card.setDiscountRate(10);
-				}else if (discountGrow < 10) {
-					newDiscount = (int) discountGrow;
-					card.setDiscountRate(card.getDiscountRate() + newDiscount);
+		try {
+			if(card.getType().equalsIgnoreCase("bronze")) {
+				if(card.getTurnover() < 100) {
+					card.setDiscountRate(0);
+				} else if(card.getTurnover()>=100 && card.getTurnover()<=300) {
+					card.setDiscountRate(1);
+				}else if(card.getTurnover()>300) {
+					card.setDiscountRate(2.5);
 				}
-			} else if(card.getTurnover() < 100) {
-				card.setTurnover(2);
 			}
+			if(card.getType().equalsIgnoreCase("silver")) {
+				if(card.getTurnover() < 300) {
+					card.setDiscountRate(2);
+				} else if(card.getTurnover() >= 300) {
+					card.setDiscountRate(3.5);
+				}
+			}
+			if(card.getType().equalsIgnoreCase("gold")) {
+				double discountGrow;
+				int newDiscount;
+				discountGrow = card.getTurnover() /100;
+				card.setDiscountRate(2);
+				if(card.getTurnover() >= 100) {
+					if(discountGrow >= 10) {
+						card.setDiscountRate(10);
+					}else if (discountGrow < 10) {
+						newDiscount = (int) discountGrow;
+						card.setDiscountRate(card.getDiscountRate() + newDiscount);
+					}
+				} else if(card.getTurnover() < 100) {
+					card.setTurnover(2);
+				}
+			}
+			return card;
+		}catch (Exception e) {
+			System.out.println("Something went rwong when initializing discount rate");
 		}
 		return card;
 	}
