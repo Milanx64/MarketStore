cabd440ec44f13a6136b934d4fa8bce80f0b10eb
Milanx64 <markovic37m@gmail.com>
2020-07-06 12:00:38 +0200

    Application entry point. Containing method main and method to calculate and print the output.

---
+++ MarketStore/src/MarketStore.java
@@ -0,0 +1,35 @@
+
+public class MarketStore {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Customer customer = new Customer(150);
+		Card card = new Card(customer,0,"bronze");
+		card = card.initializeDiscountRate(card);
+		calculatePerchs(card);
+		
+		Customer customerTwo = new Customer(850);
+		Card cardTwo = new Card(customerTwo,600,"silver");
+		cardTwo = cardTwo.initializeDiscountRate(cardTwo);
+		calculatePerchs(cardTwo);
+		
+		Customer customerThree = new Customer(1300);
+		Card cardThree = new Card(customerThree,1500,"gold");
+		cardThree = cardThree.initializeDiscountRate(cardThree);
+		calculatePerchs(cardThree);
+
+	}
+	
+	public static void calculatePerchs(Card card) {
+		double discount = card.getCustomer().getValueOfPurchase() * (card.getDiscountRate()/100);
+		double total = card.getCustomer().getValueOfPurchase() - discount;
+		
+		System.out.println("Purchase value: " + card.getCustomer().getValueOfPurchase());
+		System.out.println("Discount rate : " + card.getDiscountRate());
+		System.out.println("Discount      : " + discount);
+		System.out.println("Total         : " + total);
+		
+	}
+
+}
